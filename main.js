56d769d0c64f7b782f85054f2dae7eccc9bf04bb
Aloui-khalil <56514173+Aloui-khalil@users.noreply.github.com>
2020-01-19 15:11:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const pets = [
+    { name: "Max", type: "dog", bornOn: 2018 },
+    { name: "Angel", type: "cat", bornOn: 2015 },
+    { name: "Jasper", type: "dog", bornOn: 2016 }
+  ];
+
+  const getAge = pet => {
+    return new Date().getFullYear() - pet.bornOn
+  };
+
+
+  let petsWithAge = [];
+  pets.forEach(pet => {pet.age = getAge(pet)})
+  console.log(petsWithAge);
+
+
+  const dogs = [];
+  dogs = pets.filter(pet => pet.type == "dog")
+
+  console.log(dogs);
+    
+  let jasper;
+  jasper = pets.find(pet => pet.name === "Jasper")
+
+
+  console.log("Jasper is " + jasper.age + " years old");
